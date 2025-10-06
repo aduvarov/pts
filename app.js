006dc56f9ada181a2b09fe100d0c3f71166fd34d
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-06 22:35:11 +0500

    3.18. Enums

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 "use strict";
-const skill = [1, 'Dev'];
-// const id = skill[0]
-// const skillName = skill[1]
-const [id, skillName] = skill;
-const arr = [1, 'sdf', true, true, false];
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 1] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 2] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILED"] = 3] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: 'Платёж успешен',
+    statusCode: StatusCode.SUCCESS,
+};
+// 1 - Успех
+// 2 - в процессе
+// 3 - отклонён
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+function action(status) { }
+action(StatusCode.SUCCESS);
+action(1);
+function compute() {
+    return 3;
+}
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 0] = "ADMIN";
+    Roles[Roles["USER"] = compute()] = "USER";
+})(Roles || (Roles = {}));
+const move = 0 /* Directions.UP */;
--- app.ts
+++ app.ts
@@ -1,3 +1,40 @@
-const skill: readonly [number, string] = [1, 'Dev']
+enum StatusCode {
+    SUCCESS = 1,
+    IN_PROCESS,
+    FAILED,
+}
 
-const skills: readonly string[] = ['Dev', 'DevOps']
+const res = {
+    message: 'Платёж успешен',
+    statusCode: StatusCode.SUCCESS,
+}
+
+// 1 - Успех
+// 2 - в процессе
+// 3 - отклонён
+
+if (res.statusCode === StatusCode.SUCCESS) {
+}
+
+function action(status: StatusCode) {}
+
+action(StatusCode.SUCCESS)
+action(1)
+
+function compute() {
+    return 3
+}
+
+enum Roles {
+    ADMIN,
+    USER = compute(),
+}
+
+const enum Directions {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT,
+}
+
+const move = Directions.UP
