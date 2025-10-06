7d831fac762469a77b50899fcaf270d738fe2f3f
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-06 21:29:12 +0500

    3.17. Readonly

--- app.ts
+++ app.ts
@@ -1,7 +1,3 @@
-const skill: [number, string] = [1, 'Dev']
-// const id = skill[0]
-// const skillName = skill[1]
+const skill: readonly [number, string] = [1, 'Dev']
 
-const [id, skillName] = skill
-
-const arr: [number, string, ...boolean[]] = [1, 'sdf', true, true, false]
+const skills: readonly string[] = ['Dev', 'DevOps']
