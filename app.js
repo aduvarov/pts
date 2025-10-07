29d1ddf4240ceed30cfd4d443d8a7f7c9db0fe22
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-07 15:26:59 +0500

    4.21. Union.

---
+++ app.js
@@ -0,0 +1,39 @@
+"use strict";
+function logId(id) {
+    if (typeof id === 'string') {
+        console.log(id);
+    }
+    else if (typeof id === 'number') {
+        console.log(id);
+    }
+    else {
+        console.log(id);
+    }
+}
+logId(1);
+logId('ID1');
+logId(false);
+function logError(err) {
+    if (Array.isArray(err)) {
+        console.log(err);
+    }
+    else {
+        console.log(err);
+    }
+}
+function logObject(obj) {
+    if ('a' in obj) {
+        console.log(obj.a);
+    }
+    else {
+        console.log(obj.b);
+    }
+}
+function logMultipleIds(a, b) {
+    if (a === b) {
+        console.log(a);
+    }
+    else {
+        console.log(a);
+    }
+}
