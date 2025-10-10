4a8be9ad450ae0fdfe976f94da5d670db7e448a4
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-10 17:44:47 +0500

    4.32. Приведение типов.

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 "use strict";
-const n = null;
-const n1 = null;
-function getUser() {
-    if (Math.random() > 0.5) {
-        return null;
-    }
-    else {
-        return {
-            name: 'Вася',
-        };
-    }
+let a = 5;
+let b = a.toString();
+let e = new String(a); // String
+let e2 = new String(a).valueOf(); // string
+let f = new Boolean(a).valueOf(); // boolean
+let c = 'sdfs';
+let d = parseInt(c);
+const user = {
+    name: 'Вася',
+    email: 'vasiliy@yandex.ru',
+    login: 'vasya',
+};
+const user2 = {
+    name: 'Вася',
+    email: 'vasiliy@yandex.ru',
+    login: 'vasya',
+};
+// valid но не рекомендуется
+const user3 = {
+    name: 'Вася',
+    email: 'vasiliy@yandex.ru',
+    login: 'vasya',
+};
+const admin = Object.assign(Object.assign({}, user), { role: 1 }); // не рекомендуется
+function userToAdmin(user) {
+    return {
+        name: user.name,
+        role: 1,
+    };
 }
-const user = getUser();
-if (user) {
-    const uname = user.name; //strictNullChecks
-}
+const admin2 = userToAdmin(user);
--- app.ts
+++ app.ts
@@ -1,27 +1,49 @@
-const n: null = null
-const n1: any = null
+let a = 5
+let b: string = a.toString()
+let e = new String(a) // String
+let e2 = new String(a).valueOf() // string
+let f: boolean = new Boolean(a).valueOf() // boolean
 
-// strictNullChecks
-// const n2: number = null
-// const n3: string = null
-// const n4: boolean = null
-// const n5: undefined = null
+let c = 'sdfs'
+let d: number = parseInt(c)
 
 interface User {
     name: string
+    email: string
+    login: string
 }
 
-function getUser() {
-    if (Math.random() > 0.5) {
-        return null
-    } else {
-        return {
-            name: 'Вася',
-        } as User
+const user: User = {
+    name: 'Вася',
+    email: 'vasiliy@yandex.ru',
+    login: 'vasya',
+}
+
+const user2 = {
+    name: 'Вася',
+    email: 'vasiliy@yandex.ru',
+    login: 'vasya',
+} as User
+
+// valid но не рекомендуется
+const user3 = <User>{
+    name: 'Вася',
+    email: 'vasiliy@yandex.ru',
+    login: 'vasya',
+}
+
+interface Admin {
+    name: string
+    role: number
+}
+
+const admin: Admin = { ...user, role: 1 } // не рекомендуется
+
+function userToAdmin(user: User): Admin {
+    return {
+        name: user.name,
+        role: 1,
     }
 }
 
-const user = getUser()
-if (user) {
-    const uname = user.name //strictNullChecks
-}
+const admin2: Admin = userToAdmin(user)
