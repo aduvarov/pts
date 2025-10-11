419940cbf80e568cb273d5b2a1242c130e966e8a
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-11 12:59:34 +0500

    5.3. Конструктор

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 "use strict";
 class User {
-    constructor(name) {
-        //strictPropertyInitialization: false
-        this.name = name;
+    constructor(ageOrName, age) {
+        if (typeof ageOrName === 'string') {
+            this.name = ageOrName;
+        }
+        else if (typeof ageOrName === 'number') {
+            this.age = ageOrName;
+        }
+        if (typeof age === 'number') {
+            this.age = age;
+        }
     }
 }
 const user = new User('Вася');
-console.log(user);
-user.name = 'Петя';
-console.log(user);
-class Admin {
-}
-const admin = new Admin();
-admin.role = 1;
+const user2 = new User();
+const user3 = new User(33);
+const user4 = new User('Петя', 18);
+console.log(user4);
--- app.ts
+++ app.ts
@@ -1,20 +1,25 @@
 class User {
     name: string
+    age: number
 
-    constructor(name: string) {
-        this.name = name
+    constructor()
+    constructor(name: string)
+    constructor(age: number)
+    constructor(name: string, age: number)
+    constructor(ageOrName?: string | number, age?: number) {
+        if (typeof ageOrName === 'string') {
+            this.name = ageOrName
+        } else if (typeof ageOrName === 'number') {
+            this.age = ageOrName
+        }
+        if (typeof age === 'number') {
+            this.age = age
+        }
     }
 }
 
 const user = new User('Вася')
-console.log(user)
-user.name = 'Петя'
-console.log(user)
-
-class Admin {
-    //strictPropertyInitialization: false
-    role: number
-}
-
-const admin = new Admin()
-admin.role = 1
+const user2 = new User()
+const user3 = new User(33)
+const user4 = new User('Петя', 18)
+console.log(user4)
