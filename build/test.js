fd8bac6ce644580c9186254ac17563dfdc563d76
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-13 21:28:10 +0500

    7.4. Упражнение. Функция преобразования в строку.

---
+++ build/test.js
@@ -0,0 +1,43 @@
+"use strict";
+/*
+        Необходимо написать функцию toString, которая принимает любой тип
+        и возвращает его строковое представление. Если не может, то
+        возвращает undefined
+*/
+function toString(data) {
+    let result = undefined;
+    switch (typeof data) {
+        case 'string':
+            result = data;
+            break;
+        case 'number':
+            result = data.toString();
+            break;
+        case 'bigint':
+            result = data.toString();
+            break;
+        case 'boolean':
+            result = data.toString();
+            break;
+        case 'symbol':
+            result = data.toString();
+            break;
+        case 'function':
+            result = data.toString();
+            break;
+        case 'object':
+            if (Array.isArray(data)) {
+                result = data.toString();
+            }
+            else if (data === null) {
+                result = 'null';
+            }
+            else {
+                result = JSON.stringify(data);
+            }
+            break;
+    }
+    return result;
+}
+const res = toString(undefined);
+console.log('res ==> ', res);
