4e3b3c21f42cd5f3e92b2644db547385fd798149
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-13 22:57:36 +0500

    7.5. Использование в типах.

---
+++ build/app.js
@@ -0,0 +1,25 @@
+"use strict";
+function logMiddleware(data) {
+    console.log(data);
+    return data;
+}
+const res = logMiddleware('10');
+function getSplitedHalf(data) {
+    const l = data.length / 2;
+    return data.splice(0, l);
+}
+getSplitedHalf([1, 3, 4]);
+const split = getSplitedHalf;
+const split2 = getSplitedHalf;
+const logLine = {
+    timeStamp: new Date(),
+    data: {
+        a: 1,
+    },
+};
+const logLine2 = {
+    timeStamp: new Date(),
+    data: {
+        a: 1,
+    },
+};
