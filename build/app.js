1a081440a14b71a8fc7f19187986e743c4e55a7e
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-15 18:31:40 +0500

    8.4. Typeof.

---
+++ build/app.js
@@ -0,0 +1,24 @@
+"use strict";
+let strOrNum = 5;
+if (Math.random() > 0.5) {
+    strOrNum = 5;
+}
+else {
+    strOrNum = 'str';
+}
+if (typeof strOrNum === 'string') {
+    console.log(strOrNum);
+}
+else {
+    console.log(strOrNum);
+}
+let str2OrNum;
+const user = {
+    name: 'Вася',
+    lastname: 'Пупкин',
+};
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+})(Direction || (Direction = {}));
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+let strOrNum: string | number = 5
+
+if (Math.random() > 0.5) {
+    strOrNum = 5
+} else {
+    strOrNum = 'str'
+}
+
+if (typeof strOrNum === 'string') {
+    console.log(strOrNum)
+} else {
+    console.log(strOrNum)
+}
+
+let str2OrNum: typeof strOrNum
+
+const user = {
+    name: 'Вася',
+    lastname: 'Пупкин',
+}
+
+type keyOfUser = keyof typeof user
+
+enum Direction {
+    Up,
+    Down,
+}
+
+type d = keyof typeof Direction
