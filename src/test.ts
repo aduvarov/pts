78f0041e5435dfbc5c0b61b1a2a67c58ac8cbcd4
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-18 17:39:57 +0500

    10.7. Упражнение - Декоратор перехвата ошибок.

--- src/test.ts
+++ src/test.ts
@@ -6,8 +6,7 @@ interface IUserService {
 class UserService implements IUserService {
     users: number = 1000
 
-    @Log
-    @LogF() // Фабрика
+    @Catch({ rethrow: false })
     getUsersInDatabase(): number {
         throw new Error('Ошибка')
     }
@@ -28,17 +27,24 @@ function Log(
     }
 }
 
-function LogF() {
+function Catch({ rethrow }: { rethrow: boolean } = { rethrow: true }) {
     return (
         target: Object,
-        propertyKey: string | symbol,
+        _: string | symbol,
         descriptor: TypedPropertyDescriptor<(...args: any[]) => any>
     ): TypedPropertyDescriptor<(...args: any[]) => any> | void => {
-        console.log(target)
-        console.log(propertyKey)
-        console.log(descriptor)
-        descriptor.value = () => {
-            console.log('no error')
+        const method = descriptor.value
+        descriptor.value = async (...args: [any]) => {
+            try {
+                return await method?.apply(target, args)
+            } catch (e) {
+                if (e instanceof Error) {
+                    console.log(e.message)
+                    if (rethrow) {
+                        throw e
+                    }
+                }
+            }
         }
     }
 }
