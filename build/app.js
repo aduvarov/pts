18f219d9fbf6a03a4367fadf7e2d0c501097220a
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-20 19:29:33 +0500

    11.4. Модульность на frontend.

--- build/app.js
+++ build/app.js
@@ -1,4 +1,4 @@
 // import { A } from './module/app2'
-import { a } from './module/app2.js'
+import { a } from './module/app2.js';
 // console.log(A.a)
-console.log(a)
+console.log(a);
--- build/module/app2.js
+++ build/module/app2.js
@@ -1,13 +1,7 @@
 // export namespace A {
 //     export const a = 5
-
 //     export interface B {
 //         c: number
 //     }
 // }
-
-export const a = 5
-
-export interface B {
-    c: number
-}
+export const a = 5;
