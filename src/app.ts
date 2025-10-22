7d806927c37eacdb13d1646c018e8df15d0030d3
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-10-22 07:32:08 +0500

    13.2. Bridge.

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+interface IProvider {
+    sendMessage(message: string): void
+    connect(config: unknown): void
+    disconnect(): void
+}
+
+class TelegramProvider implements IProvider {
+    sendMessage(message: string): void {
+        console.log(message)
+    }
+    connect(config: string): void {
+        console.log(config)
+    }
+    disconnect(): void {
+        console.log('Disconected TG')
+    }
+}
+
+class WhatsUpProvider implements IProvider {
+    sendMessage(message: string): void {
+        console.log(message)
+    }
+    connect(config: string): void {
+        console.log(config)
+    }
+    disconnect(): void {
+        console.log('Disconected WhatsUp')
+    }
+}
+
+class NotificationSender {
+    constructor(private provider: IProvider) {}
+
+    send() {
+        this.provider.connect('connect')
+        this.provider.sendMessage('message')
+        this.provider.disconnect()
+    }
+}
+
+class DelayNotificationSender extends NotificationSender {
+    constructor(provider: IProvider) {
+        super(provider)
+    }
+    sendDelayed() {}
+}
+
+const sender = new NotificationSender(new TelegramProvider())
+sender.send()
+
+const sender2 = new NotificationSender(new WhatsUpProvider())
+sender2.send()
